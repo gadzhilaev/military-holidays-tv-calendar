51280ba898f97fa9fbe7bb9b911fd5dba3710992
Магомедгаджи <info@gadzhilaev.ru>
2025-12-22 19:57:37 +0300

    first commit

---
+++ app/src/main/java/com/example/military_holidays_tv_calendar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.military_holidays_tv_calendar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.tv.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.Surface
+import com.example.military_holidays_tv_calendar.ui.theme.MilitaryholidaystvcalendarTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalTvMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MilitaryholidaystvcalendarTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    shape = RectangleShape
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MilitaryholidaystvcalendarTheme {
+        Greeting("Android")
+    }
+}
